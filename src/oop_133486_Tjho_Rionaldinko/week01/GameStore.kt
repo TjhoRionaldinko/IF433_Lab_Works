d2296f2ecc091bc0b82cb89400541482c588397b
Tjho Rionaldinko <rionaldinkotjho@gmail.com>
2026-02-13 23:21:59 +0700

    Week01: (tugas 2) impplement discount logic

--- src/oop_133486_Tjho_Rionaldinko/week01/GameStore.kt
+++ src/oop_133486_Tjho_Rionaldinko/week01/GameStore.kt
@@ -3,7 +3,10 @@ package oop_133486_Tjho_Rionaldinko.week01
 fun main() {
     val gameTitle = "R.E.P.O."
     val price = 100000
+    val discount = calculateDiscount(price)
 
     println("Game: $gameTitle")
     println("Price: $price")
 }
+
+fun calculateDiscount(price: Int)= if(price > 500000) price * 20 / 100 else price * 10 / 100
