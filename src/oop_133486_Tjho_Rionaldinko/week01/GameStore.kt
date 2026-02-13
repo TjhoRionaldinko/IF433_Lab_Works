fd5647269f0df6eef1b6ff59679d587bdb94032e
Tjho Rionaldinko <rionaldinkotjho@gmail.com>
2026-02-13 23:17:27 +0700

    Week01: (tugas 2) setup GameStore file

---
+++ src/oop_133486_Tjho_Rionaldinko/week01/GameStore.kt
@@ -0,0 +1,9 @@
+package oop_133486_Tjho_Rionaldinko.week01
+
+fun main() {
+    val gameTitle = "R.E.P.O."
+    val price = 100000
+
+    println("Game: $gameTitle")
+    println("Price: $price")
+}
