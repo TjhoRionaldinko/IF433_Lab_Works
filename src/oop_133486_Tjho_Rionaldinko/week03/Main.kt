b672bcd2a754d3a92f543d325fd9b518ff661f4c
Tjho Rionaldinko <rionaldinkotjho@gmail.com>
2026-02-26 23:51:24 +0700

    week03: (task 1) implement Weapon testing in main

--- src/oop_133486_Tjho_Rionaldinko/week03/Main.kt
+++ src/oop_133486_Tjho_Rionaldinko/week03/Main.kt
@@ -1,14 +1,12 @@
 package oop_133486_Tjho_Rionaldinko.week03
 
 fun main() {
-    val e = Employee("Budi")
 
-    e.salary = -1000
-    e.salary = 5000000
-    println("Gaji: ${e.salary}")
+    val sword = Weapon("Excalibur", 100)
 
-    e.increasePerformance()
+    sword.damage = -50
+    sword.damage = 9999
 
-    println("Pajak yang harus dibayar: ${e.tax}")
-    // coment codenya sudah di hapus dari awal jadi saya selesaikan checkpoint 6 menggunakkan komentar
+    println("Damage: ${sword.damage}")
+    println("Tier: ${sword.tier}")
 }
