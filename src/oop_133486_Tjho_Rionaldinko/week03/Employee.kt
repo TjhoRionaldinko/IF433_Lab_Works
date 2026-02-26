f2dadca6c48f716b9af574e6a023c18ebdc4796e
Tjho Rionaldinko <rionaldinkotjho@gmail.com>
2026-02-26 21:35:02 +0700

    week03: fix setter using backing field

--- src/oop_133486_Tjho_Rionaldinko/week03/Employee.kt
+++ src/oop_133486_Tjho_Rionaldinko/week03/Employee.kt
@@ -2,4 +2,12 @@ package oop_133486_Tjho_Rionaldinko.week03
 
 class Employee(val name: String) {
     var salary: Int = 0
+        set(value) {
+            if(value < 0) {
+                println("ERROR: Gaji tidak boleh negatif! Di-set ke0.")
+                field = 0
+            }else{
+                field = value
+            }
+        }
 }
--- src/oop_133486_Tjho_Rionaldinko/week03/Main.kt
+++ src/oop_133486_Tjho_Rionaldinko/week03/Main.kt
@@ -1,2 +1,7 @@
 package oop_133486_Tjho_Rionaldinko.week03
 
+fun main() {
+    val e = Employee("Budi")
+    println("--- Test Error ---")
+    e.salary = 5000
+}
