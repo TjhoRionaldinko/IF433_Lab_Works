4e9b28da5afa32be6f4ffe8054dc207537fc8de4
Tjho Rionaldinko <rionaldinkotjho@gmail.com>
2026-03-05 21:47:36 +0700

    week04: test Vehicle and Car in main

---
+++ src/oop_133486_Tjho_Rionaldinko/week04/Main.kt
@@ -0,0 +1,14 @@
+package oop_133486_Tjho_Rionaldinko.week04
+
+fun main() {
+    println("--- Testing Vehicle ---")
+    val generalVehicle = Vehicle("Sepeda Onthel")
+    generalVehicle.honk()
+    generalVehicle.accelerate()
+
+    println("\n--- Testing Vehicle ---")
+    val myCar = Car("Toyota", 4)
+    myCar.openTrunk()
+    myCar.honk()
+    myCar.accelerate()
+}
