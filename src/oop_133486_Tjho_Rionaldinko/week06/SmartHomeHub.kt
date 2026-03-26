8aae700ef487a210c2de009034147896196c2b35
Tjho Rionaldinko <rionaldinkotjho@gmail.com>
2026-03-27 00:51:30 +0700

    week06: (task) implement advanced smart casting in Hub

--- src/oop_133486_Tjho_Rionaldinko/week06/SmartHomeHub.kt
+++ src/oop_133486_Tjho_Rionaldinko/week06/SmartHomeHub.kt
@@ -3,12 +3,22 @@ package oop_133486_Tjho_Rionaldinko.week06
 class SmartHomeHub() {
     val devices = mutableListOf<SmartDevice>()
     fun addDevice(device: SmartDevice) {
+        devices.add(device)
     }
 
     fun turnOffAllSwitches() {
         for (device in devices) {
             if (device is Switchable) {
                 device.turnOff()
+            }
+        }
+    }
+    fun activateSecurityMode(){
+        for (device in devices) {
+            if (device is Recordable){
+                device.startRecord()
+            } else if (device is SmartSpeaker){
+                device.playMusic("Sirine Peringatan")
             }
         }
     }
