029b35def792ca1e974f968fea11a6f1a45a0eca
Tjho Rionaldinko <rionaldinkotjho@gmail.com>
2026-03-27 00:52:46 +0700

    week06: (task) instantiate smart devices in main

--- src/oop_133486_Tjho_Rionaldinko/week06/Main.kt
+++ src/oop_133486_Tjho_Rionaldinko/week06/Main.kt
@@ -6,16 +6,20 @@ fun processCheckout(method: PaymentMethod, amount: Double) {
 }
 
 fun main() {
-    val myWatch = Smartwatch()
-    myWatch.showTime()
+//    val myWatch = Smartwatch()
+//    myWatch.showTime()
+//
+//    val myPhone = Smartphone()
+//    myPhone.turnOn()
+//
+//    val pay1 = Gopay()
+//    val pay2 = CreditCard()
+//
+//    println("\n=== TESTING CHECKOUT ===")
+//    processCheckout(pay1, 50000.0)
+//    processCheckout(pay2, 150000.0)
 
-    val myPhone = Smartphone()
-    myPhone.turnOn()
-
-    val pay1 = Gopay()
-    val pay2 = CreditCard()
-
-    println("\n=== TESTING CHECKOUT ===")
-    processCheckout(pay1, 50000.0)
-    processCheckout(pay2, 150000.0)
+    val lamp = SmartLamp("Lamp01", "Ruang Tamu")
+    val speaker = SmartSpeaker("Speaker01", "Google Nest Dapur")
+    val cctv = SmartCCTV("CCTV01", "Ezviz Garasi")
 }
