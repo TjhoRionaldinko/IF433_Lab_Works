6880b4e5a853dd6f3a7ba11295bc6bc927aad980
Tjho Rionaldinko <rionaldinkotjho@gmail.com>
2026-03-27 23:35:24 +0700

    week07: (task) test GameManager singleton in main

--- src/oop_133486_Tjho_Rionaldinko/week07/GameModels.kt
+++ src/oop_133486_Tjho_Rionaldinko/week07/GameModels.kt
@@ -1,11 +1,9 @@
 package oop_133486_Tjho_Rionaldinko.week07
 
-enum class ItemRarity {
+enum class ItemRarity(Rarity: Int) {
     COMMON(30),
     UNCOMMON(15),
     RARE(10),
     EPIC(5),
     LEGENDARY(1)
 }
-
-data class Item(val Name: String, val damage: Int, val rarity: ItemRarity)
--- src/oop_133486_Tjho_Rionaldinko/week07/Main.kt
+++ src/oop_133486_Tjho_Rionaldinko/week07/Main.kt
@@ -3,32 +3,36 @@ package oop_133486_Tjho_Rionaldinko.week07
 import javax.xml.crypto.Data
 
 fun main(){
-    println("=== TEST SINGLETON ===")
-    println("Status: ${DatabaseManager.connectionStatus}")
-    DatabaseManager.connect()
+//    println("=== TEST SINGLETON ===")
+//    println("Status: ${DatabaseManager.connectionStatus}")
+//    DatabaseManager.connect()
+//
+//    println("\n=== TEST COMPANION OBJECT ===")
+//    val client = NetworkClient.createClient()
+//    client.connect()
+//
+//    println("\n=== TEST REGULAR CLASS ===")
+//    val data1 = DataUser("Alice", 22)
+//    val data2 = DataUser("Alice", 22)
+//    println(data1)
+//    println("Sama? ${data1 == data2}")
+//
+//    val data3 = data1.copy(age = 23)
+//    println("Hasil Copy: $data3")
+//
+//    val (userName, userAge) = data1
+//    println("Destructured: $userName berumur $userAge")
+//
+//    println("\n=== TEST SEALED CLASS ===")
+//    val response: ApiResponse = ApiResponse.Success("Data berhasil ditarik!")
+//
+//    val uiMessage = when(response){
+//        is ApiResponse.Success -> "Tampilkan: ${response.data}"
+//        is ApiResponse.Error -> "Munculkan alert: ${response.message}"
+//        ApiResponse.Loading -> "Tampilkan Spinner
+//    }
 
-    println("\n=== TEST COMPANION OBJECT ===")
-    val client = NetworkClient.createClient()
-    client.connect()
-
-    println("\n=== TEST REGULAR CLASS ===")
-    val data1 = DataUser("Alice", 22)
-    val data2 = DataUser("Alice", 22)
-    println(data1)
-    println("Sama? ${data1 == data2}")
-
-    val data3 = data1.copy(age = 23)
-    println("Hasil Copy: $data3")
-
-    val (userName, userAge) = data1
-    println("Destructured: $userName berumur $userAge")
-
-    println("\n=== TEST SEALED CLASS ===")
-    val response: ApiResponse = ApiResponse.Success("Data berhasil ditarik!")
-
-    val uiMessage = when(response){
-        is ApiResponse.Success -> "Tampilkan: ${response.data}"
-        is ApiResponse.Error -> "Munculkan alert: ${response.message}"
-        ApiResponse.Loading -> "Tampilkan Spinner"
-    }
+    println("\n=== Test GameManager ===")
+    GameManager.startGame()
+    GameManager.startGame()
 }
