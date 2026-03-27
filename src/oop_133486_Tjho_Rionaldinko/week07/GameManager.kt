2d60ce64c58bf1c978809b8361a556bae2bce9b4
Tjho Rionaldinko <rionaldinkotjho@gmail.com>
2026-03-27 11:16:31 +0700

    week07: (task) create GameManager singleton

---
+++ src/oop_133486_Tjho_Rionaldinko/week07/GameManager.kt
@@ -0,0 +1,14 @@
+package oop_133486_Tjho_Rionaldinko.week07
+
+object GameManager {
+    var isGameRunning: Boolean = false
+
+    fun startGame() {
+        if (isGameRunning) {
+            println("Game sudah berjalan! Mencegah instansiasi ganda.")
+        } else {
+            isGameRunning = true
+            println("Memulai Game Engine...")
+        }
+    }
+}
