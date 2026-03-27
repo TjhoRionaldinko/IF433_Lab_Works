96cbe6a6aad5de6c6c0d7afbfd5df237cfea6c9b
Tjho Rionaldinko <rionaldinkotjho@gmail.com>
2026-03-27 11:30:28 +0700

    week07: (task) create BattleState sealed class hierarchy

---
+++ src/oop_133486_Tjho_Rionaldinko/week07/BattleEvent.kt
@@ -0,0 +1,8 @@
+package oop_133486_Tjho_Rionaldinko.week07
+
+sealed class BattleState () {
+    data class MonsterEncounter(val monsterName: String): BattleState()
+    data class LootDropped(val item: Item): BattleState()
+    data class GameOver(val reason: String): BattleState()
+    object SafeZone: BattleState()
+}
