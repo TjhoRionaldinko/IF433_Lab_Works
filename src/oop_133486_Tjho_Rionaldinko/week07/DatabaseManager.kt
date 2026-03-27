9c0480d9f62a6916c4b7cef7c0a002ccc47aff6b
Tjho Rionaldinko <rionaldinkotjho@gmail.com>
2026-03-27 10:49:16 +0700

    week07: implement companion object as factory in networkClient

--- src/oop_133486_Tjho_Rionaldinko/week07/DatabaseManager.kt
+++ src/oop_133486_Tjho_Rionaldinko/week07/DatabaseManager.kt
@@ -1,2 +1,10 @@
 package oop_133486_Tjho_Rionaldinko.week07
 
+object DatabaseManager {
+    var connectionStatus: String = "Disconnected"
+
+    fun connect() {
+        connectionStatus = "Connected to Server"
+        println("Database is ready.")
+    }
+}
--- src/oop_133486_Tjho_Rionaldinko/week07/Main.kt
+++ src/oop_133486_Tjho_Rionaldinko/week07/Main.kt
@@ -1,2 +1,6 @@
 package oop_133486_Tjho_Rionaldinko.week07
 
+fun main(){
+    val client = NetworkClient("https://api.umn.ac.id")
+    client.connect()
+}
