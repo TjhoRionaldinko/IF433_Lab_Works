540a68981e7d3c263f1006a318990ace169b369f
Tjho Rionaldinko <rionaldinkotjho@gmail.com>
2026-03-28 16:24:30 +0700

    week07: (task) simulate rarity and factory instantiation

--- src/oop_133486_Tjho_Rionaldinko/week07/GameExecutor.kt
+++ src/oop_133486_Tjho_Rionaldinko/week07/GameExecutor.kt
@@ -3,7 +3,7 @@ package oop_133486_Tjho_Rionaldinko.week07
 fun processEvent(event: BattleState){
     val msg = when(event){
         is BattleState.MonsterEncounter -> "You have encountered a ${event.monsterName}!"
-        is BattleState.LootDropped -> "You found a ${event.item.Name}!"
+        is BattleState.LootDropped -> "You found a ${event.item.name}!"
         is BattleState.SafeZone -> "You are in a safe zone. Take a rest!"
         is BattleState.GameOver -> "Game over! ${event.reason}"
     }
--- src/oop_133486_Tjho_Rionaldinko/week07/GameModels.kt
+++ src/oop_133486_Tjho_Rionaldinko/week07/GameModels.kt
@@ -1,9 +1,13 @@
-package oop_133486_Tjho_Rionaldinko.week07
-
-enum class ItemRarity(Rarity: Int) {
+enum class ItemRarity(val dropChance: Int) {
     COMMON(30),
     UNCOMMON(15),
     RARE(10),
     EPIC(5),
     LEGENDARY(1)
 }
+
+data class Item(
+    val name: String,
+    val damage: Int,
+    val rarity: ItemRarity
+)
