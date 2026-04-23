4c4dc61cb0a4a1d213855025af8ffc6348ceae5a
Tjho Rionaldinko <rionaldinkotjho@gmail.com>
2026-04-23 23:25:29 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_133486_Tjho_Rionaldinko/week08/JavaPaymentService.java
@@ -0,0 +1,8 @@
+package oop_133486_Tjho_Rionaldinko.week08;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+
+}
