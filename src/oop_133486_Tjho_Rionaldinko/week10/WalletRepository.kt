81ccfed0353f22477b90490f388f0bcd8069437d
Tjho Rionaldinko <rionaldinkotjho@gmail.com>
2026-05-15 09:57:07 +0700

    week10: (task) implement search logic in repository

--- src/oop_133486_Tjho_Rionaldinko/week10/WalletRepository.kt
+++ src/oop_133486_Tjho_Rionaldinko/week10/WalletRepository.kt
@@ -1,7 +1,11 @@
 package oop_133486_Tjho_Rionaldinko.week10
 
-class WalletRepository<T> {
+class WalletRepository<T: Any> {
     private val items = mutableListOf<T>()
     fun add(item: T) = items.add(item)
     fun getAll(): List<T> = return items
+
+    fun pencarian(items: List<T>): List<T> {
+        return items.filter {it == items }
+    }
 }
