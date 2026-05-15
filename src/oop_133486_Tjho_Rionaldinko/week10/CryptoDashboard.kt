becf11286fd3cda4cb3bc8f8d2848fd8ae639db6
Tjho Rionaldinko <rionaldinkotjho@gmail.com>
2026-05-15 10:08:13 +0700

    week10: (task) populate coin repository

--- src/oop_133486_Tjho_Rionaldinko/week10/CryptoDashboard.kt
+++ src/oop_133486_Tjho_Rionaldinko/week10/CryptoDashboard.kt
@@ -2,4 +2,7 @@ package oop_133486_Tjho_Rionaldinko.week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+    coinRepo.add(Coin("BTC", 10000.0))
+    coinRepo.add(Coin("ETH", 20000.0))
+    coinRepo.add(Coin("USDT", 30000.0))
 }
